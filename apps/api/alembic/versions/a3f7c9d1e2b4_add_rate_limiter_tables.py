db211a80791a6b1ffa95780b64ea184117c85719
suguanYang <wangbinqi77@gmail.com>
2026-03-05 18:34:08 +0800

    bugfix: ensure db migration order

--- apps/api/alembic/versions/a3f7c9d1e2b4_add_rate_limiter_tables.py
+++ apps/api/alembic/versions/a3f7c9d1e2b4_add_rate_limiter_tables.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'a3f7c9d1e2b4'
-down_revision: Union[str, Sequence[str], None] = 'b2c3d4e5f6a8'
+down_revision: Union[str, Sequence[str], None] = 'jkl012mno345'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
